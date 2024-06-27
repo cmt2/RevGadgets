819913b7c82c49e8340a1728fd7a1fa0c0a23bd1
Anjalithapaa <anjali.thapa@selu.edu>
2024-06-27 13:38:52 -0500

    test

--- R/readTrace.R
+++ R/readTrace.R
@@ -1,4 +1,34 @@
-# Load necessary libraries
+#' Read trace
+#'
+#' Reads in MCMC log files or JSON files
+#'
+#' Reads in one or multiple MCMC log files from the same analysis
+#' and discards a user-specified burn-in, compatible with multiple monitor
+#' types. If the trace contains vectors of vectors and the user does not specify
+#' format = "complex", readTrace() will read in those columns as factors
+#' rather than as numeric vectors.
+#'
+#' @param paths (vector of character strings; no default) File path(s) to trace
+#' file.
+#' @param format (single character string; default = "simple") Indicates type of
+#' MCMC trace, complex indicates cases where trace contains vectors of vectors/
+#' matrices - mnStochasticVariable monitor will sometimes be of this type.
+#' @param delim (single character string; default = "\t") Delimiter of file.
+#' @param burnin (single numeric value; default = 0.1) Fraction of generations
+#' to discard (if value provided is between 0 and 1) or number of generations
+#' (if value provided is greater than 1).
+#' @param check.names (logical; default = FALSE) Passed to utils::read.table();
+#' indicates if utils::read.table() should check column names and replace
+#' syntactically invalid characters.
+#' @param ... (various) Additional arguments passed to utils::read.table().
+#'
+#' @return List of dataframes (of length 1 if only 1 log file provided).
+#'
+#' @export
+
+
+
+
 library(tidyverse)
 library(jsonlite)
 library(dplyr)
@@ -44,7 +74,13 @@ readAndParseJSON <- function(file) {
 }
 
 # Function to read trace files
-readTrace <- function(paths, format = "simple", delim = "\t", burnin = 0.1, check.names = FALSE, ...) {
+readTrace <- function(paths,
+                      format = "simple",
+                      delim = "\t",
+                      burnin = 0.1, 
+                      check.names = FALSE, ...)
+  {
+  
   # Enforce argument matching
   if (!is.character(paths)) {
     stop("All paths must be character strings.")
